9592ae3db7bf50498e0c771b2d3a34acd9f63da5
Lain157 <105891295+Lain157@users.noreply.github.com>
2023-04-01 16:40:09 +0200

    Updated structure of TestEnvironment()  - Created random_circle_position() function  - Moved and updated record_answers() from class User()  - Moved arguments of run() from class TestA() to __init__() of class TestEnvironment  - current_user is now an attribute of the class TestEnvironment()  - device is now an attribute of the class TestEnvironment()

--- Tests/test_environment.py
+++ Tests/test_environment.py
@@ -1,15 +1,23 @@
 import pygame
 from pygame import mixer
 from abc import ABC, abstractmethod
-import os
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import Button
+import random
+from datetime import datetime
+import os
+
+# ----------------------------------------------------------------------------------------------  Import custom modules:
+from Database import user_database
 
 
-# ---------------------------------------------------------------------------------------------------------      Classes
+# ------------------------------------------------------------------------------------------------------------  Classes:
 # Abstract class defining test environments
 class TestEnvironment(ABC):
-    def __init__(self):
+    def __init__(self, device=None, current_user=None):
+        # Current user
+        self.current_user = current_user
+        self.device = device
 
         # Test parameters
         self.test_duration = 240000 / 20  # Test duration in ms (4 min by default)
@@ -85,6 +93,61 @@ class TestEnvironment(ABC):
         )
         pygame.event.post(button_event)
 
+    def search_for_input_device(self):
+        # Buttons have no hardware representation
+        white_button = None
+        yellow_button = None
+        green_button = None
+        blue_button = None
+        red_button = None
+        up_button = None
+        down_button = None
+        left_pedal = None
+        right_pedal = None
+        buttons = [white_button, yellow_button, green_button, blue_button,
+                   red_button, up_button, down_button, left_pedal, right_pedal]
+
+        # User chose to use control pane to control the test
+        if self.device is not None and self.device.input_device == "CONTROL PANEL":
+            # Protect if panel IP address is not found
+            try:
+                # Create a connection to the Raspberry Pi remote GPIO
+                factory = PiGPIOFactory(host=self.device.device_ip)
+
+                # Confirm connection with the panel
+                panel_detected = True
+
+                # Declare buttons and pedals based on their hardware representation
+                white_button = Button(18, pin_factory=factory, pull_up=False)
+                yellow_button = Button(14, pin_factory=factory, pull_up=False)
+                green_button = Button(15, pin_factory=factory, pull_up=False)
+                blue_button = Button(22, pin_factory=factory, pull_up=False)
+                red_button = Button(27, pin_factory=factory, pull_up=False)
+                up_button = Button(23, pin_factory=factory, pull_up=False)
+                down_button = Button(17, pin_factory=factory, pull_up=False)
+                left_pedal = Button(24, pin_factory=factory, pull_up=False)
+                right_pedal = Button(25, pin_factory=factory, pull_up=False)
+
+                buttons = [white_button, yellow_button, green_button, blue_button,
+                           red_button, up_button, down_button, left_pedal, right_pedal]
+
+                print("Panel was successfully detected")
+
+                return panel_detected, buttons
+
+            # Return to menu if panel is not found
+            except OSError:
+
+                panel_detected = False
+                print("Panel not detected")
+                return panel_detected, buttons
+
+        # User chose keyboard to control the test
+        else:
+            panel_detected = False
+
+        return panel_detected, buttons
+
     # Define stimulus which is being presented during the test
     def stimulus(self, question_set_index, circle_position, stimulus_adaptive_delay_ms):
 
@@ -127,61 +190,42 @@ class TestEnvironment(ABC):
         elif question_set_index == "low_tone":
             self.low_tone.play(loops=0, maxtime=int(stimulus_adaptive_delay_ms), fade_ms=10)
 
-    @staticmethod
-    def search_for_input_device(input_device, device_ip):
-        # Buttons have no hardware representation
-        white_button = None
-        yellow_button = None
-        green_button = None
-        blue_button = None
-        red_button = None
-        up_button = None
-        down_button = None
-        left_pedal = None
-        right_pedal = None
-        buttons = [white_button, yellow_button, green_button, blue_button,
-                   red_button, up_button, down_button, left_pedal, right_pedal]
+    def random_circle_position(self):
+        position = [random.randint(0 + self.circle_size * 3, self.main_window.get_width() - self.circle_size * 3),
+                    random.randint(0 + self.circle_size * 3, self.main_window.get_height() - self.circle_size * 3)]
+        return position
 
-        # User chose to use control pane to control the test
-        if input_device == "CONTROL PANEL":
-            # Protect if panel IP address is not found
-            try:
-                # Create a connection to the Raspberry Pi remote GPIO
-                factory = PiGPIOFactory(host=device_ip)
+    # Method that initialises recording of the answers for the upcoming test
+    def record_answers(self, test_form):
+        username = "Guest"
+        score_id = None
 
-                # Confirm connection with the panel
-                panel_detected = True
+        # Check if a user was given
+        if self.current_user is not None:
+            username = self.current_user.user_name
 
-                # Declare buttons and pedals based on their hardware representation
-                white_button = Button(18, pin_factory=factory, pull_up=False)
-                yellow_button = Button(14, pin_factory=factory, pull_up=False)
-                green_button = Button(15, pin_factory=factory, pull_up=False)
-                blue_button = Button(22, pin_factory=factory, pull_up=False)
-                red_button = Button(27, pin_factory=factory, pull_up=False)
-                up_button = Button(23, pin_factory=factory, pull_up=False)
-                down_button = Button(17, pin_factory=factory, pull_up=False)
-                left_pedal = Button(24, pin_factory=factory, pull_up=False)
-                right_pedal = Button(25, pin_factory=factory, pull_up=False)
+            # Check if the user is in the user database
+            user_in_database = user_database.select_current_user(self.current_user.user_id)
 
-                buttons = [white_button, yellow_button, green_button, blue_button,
-                           red_button, up_button, down_button, left_pedal, right_pedal]
+            # Record answers only for users tracked in User Table
+            if user_in_database is not None:
+                # Connect to existing score table or create one
+                user_database.create_score_table()
 
-                print("Panel was successfully detected")
+                # Connect to existing score table or create one
+                user_database.create_answer_table()
 
-                return panel_detected, buttons
+                # Make Score Table entry for the current test and return the ID of the current score
+                score_id = user_database.insert_into_score_table(
+                    test_form,
+                    datetime.now().strftime("%d/%m/%Y %H:%M"),
+                    self.current_user.user_id)
 
-            # Return to menu if panel is not found
-            except OSError:
+                # Clean Score Table of any unwanted answers before recording
+                user_database.delete_answers(score_id)
 
-                panel_detected = False
-                print("Panel not detected")
-                return panel_detected, buttons
-
-        # User chose keyboard to control the test
-        else:
-            panel_detected = False
-
-        return panel_detected, buttons
+                return username, score_id  # ID of the score which is going to receive answers from the test
+        return username, score_id  # Returns None -> answers are not being recorded
 
     def scan_for_pressed_buttons(self, buttons, panel_detected=False):
         # Scan for buttons only if the control panel is being used to control the test
@@ -220,5 +264,5 @@ class TestEnvironment(ABC):
 
     # Method that starts the test
     @abstractmethod
-    def run(self, current_user=None, input_device="KEYBOARD", device_ip=None, phase="Instructions"):
+    def run(self, phase="Instructions"):
         pass
